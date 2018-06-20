605c6cfa310e60ccc395148b44c25587744b6e12
cb <charley.beigbeder@esc-pau.net>
2018-06-20 14:08:07 +0200

    fgh

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ end
 activate :sprockets
 
 activate :i18n, :mount_at_root => false
-# activate :i18n, :mount_at_root => :fr
+#activate :i18n, :mount_at_root => :fr
 
 page '/*.xml', layout: false
 page '/*.json', layout: false
@@ -25,4 +25,3 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
-
